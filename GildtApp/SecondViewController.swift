9837821dd0e759aade740bc7541a97639bfb3f78
Jeroen Besse <jeroen.b.k@hotmail.com>
2018-11-13 14:58:19 +0100

    init new project

---
+++ GildtApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GildtApp
+//
+//  Created by Jeroen Besse on 11/13/18.
+//  Copyright © 2018 Gildt. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
