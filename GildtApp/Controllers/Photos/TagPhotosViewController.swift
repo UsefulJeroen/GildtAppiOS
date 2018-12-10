c7f311eec190e6b3b63b6b4221c3fa168b9da5ef
Jeroen Besse <jeroen.b.k@hotmail.com>
2018-12-10 17:45:36 +0100

    added tagcontroller

---
+++ GildtApp/Controllers/Photos/TagPhotosViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TagPhotosViewController.swift
+//  GildtApp
+//
+//  Created by Jeroen Besse on 10/12/2018.
+//  Copyright © 2018 Gildt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TagPhotosViewController: UICollectionViewController {
+    
+    let itemsPerRow: CGFloat = 2
+    let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    let photos: [Photo] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+    }
+    
+    func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UINib(nibName: "TagCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "TagCollectionViewCell")
+    }
+}
