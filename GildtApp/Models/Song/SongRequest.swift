8d3a21a9df4246586877a8dacbd82abfbe63e042
Jeroen Besse <jeroen.b.k@hotmail.com>
2018-12-22 22:24:30 +0100

    tried making an encodable didvote enum, but will try again in the future 😈

--- GildtApp/Models/Song/SongRequest.swift
+++ GildtApp/Models/Song/SongRequest.swift
@@ -15,6 +15,7 @@ struct SongRequest: Codable {
     let title: String
     var votes: Int
     let userId: Int
+    let didVote: DidVote
     var row: Int?
     
     enum CodingKeys: String, CodingKey {
@@ -23,6 +24,7 @@ struct SongRequest: Codable {
         case title = "title"
         case votes = "votes"
         case userId = "user_id"
+        case didVote = "did_vote"
     }
     
     //for new songrequest on device
@@ -34,3 +36,79 @@ struct SongRequest: Codable {
         self.userId = userId
     }
 }
+
+enum DidVote:Int, Decodable {
+    case downvote = -1
+    case no = 0
+    case upvote = 1
+}
+
+/////
+
+//enum DidVote<A: Codable, B: Codable, C: Codable> {
+//    case downvote(A)
+//    case nope(B)
+//    case upvote(C)
+//}
+//
+//extension DidVote: Encodable {
+//    enum CodingKeys: CodingKey {
+//        case downvote
+//        case nope
+//        case upvote
+//    }
+//
+//    func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        switch self {
+//        case .downvote(let value):
+//            try container.encode(value, forKey: .downvote)
+//        case .nope(let value):
+//            try container.encode(value, forKey: .nope)
+//        case .upvote(let value):
+//            try container.encode(value, forKey: .upvote)
+//        }
+//    }
+//}
+//
+//extension DidVote: Decodable {
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        do {
+//            let downvote = try container.decode(A.self, forKey: .downvote)
+//            self = .downvote(downvote)
+//        } catch {
+//            do {
+//                let nope = try container.decode(B.self, forKey: .nope)
+//                self = .nope(nope)
+//            } catch {
+//                let upvote = try container.decode(C.self, forKey: .upvote)
+//                self = .upvote(upvote)
+//            }
+//        }
+//    }
+//}
+
+/////
+
+//protocol EnumEncodable: RawRepresentable, Encodable {
+//    static var defaultEncoderValue: Self { get }
+//}
+//
+//extension EnumEncodable where RawValue: Encodable {
+//    init(from encoder: Encoder) throws {
+//        var value = try encoder.singleValueContainer().encode(RawValue.self)
+//        self = Self(rawValue: value) ?? Self.defaultDecoderValue
+//    }
+//}
+//
+//enum DidVoteEnum: Int, EnumEncodable {
+//    static var defaultEncoderValue: DidVoteEnum = .other
+//
+//    //static let defaultDecoderValue: DidVoteEnum = .other
+//
+//    case downvote = -1
+//    case nope = 0
+//    case upvote = 1
+//    case other
+//}
