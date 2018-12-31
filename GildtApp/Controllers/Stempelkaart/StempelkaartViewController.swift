8d62327cf82a16898559431fc8d7129ba8b39ebd
WouterJanson <wouterjanson@icloud.com>
2018-12-31 15:28:33 +0100

    First version QR scanner and stamp claim

--- GildtApp/Controllers/Stempelkaart/StempelkaartViewController.swift
+++ GildtApp/Controllers/Stempelkaart/StempelkaartViewController.swift
@@ -6,9 +6,11 @@
 //  Copyright © 2018 Gildt. All rights reserved.
 //
 
+import AVFoundation
 import Foundation
 import UIKit
 import CoreLocation
+import QRCodeReader
 
 class StempelkaartViewController : UIViewController, CLLocationManagerDelegate {
     var stamps: [Stamp] = []
@@ -18,6 +20,18 @@ class StempelkaartViewController : UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var ClaimButton: UIButton!
     
     @IBOutlet weak var CollectionView: UICollectionView!
+
+    lazy var readerVC: QRCodeReaderViewController = {
+        let builder = QRCodeReaderViewControllerBuilder {
+            $0.reader                  = QRCodeReader(metadataObjectTypes: [.qr], captureDevicePosition: .back)
+            $0.cancelButtonTitle       = "Sluiten"
+            $0.showTorchButton         = true
+            $0.preferredStatusBarStyle = .lightContent
+            $0.reader.stopScanningWhenCodeIsFound = false
+        }
+        return QRCodeReaderViewController(builder: builder)
+    }()
+
     
     override func viewDidLoad() {
         Container.layer.cornerRadius = 10
@@ -76,15 +90,26 @@ class StempelkaartViewController : UIViewController, CLLocationManagerDelegate {
         if status == .authorizedWhenInUse && CLLocationManager.locationServicesEnabled() {
             manager.requestLocation()
             let location = manager.location
+//            let gildtLocation = CLCircularRegion.init(
+//                center: CLLocationCoordinate2D.init(
+//                    latitude: 52.3775974,
+//                    longitude: 4.6333507),
+//                radius: CLLocationDistance.init(exactly: 50)!,
+//                identifier: "Gildt")
             let gildtLocation = CLCircularRegion.init(
                 center: CLLocationCoordinate2D.init(
-                    latitude: 52.3775974,
-                    longitude: 4.6333507),
-                radius: CLLocationDistance.init(exactly: 50)!,
+                    latitude: 52.4912172,
+                    longitude: 4.649962),
+                radius: CLLocationDistance.init(exactly: 500)!,
                 identifier: "Gildt")
             
             if gildtLocation.contains((location?.coordinate)!){
                 print("At 't Gildt")
+                if checkScanPermissions() {
+                    readerVC.modalPresentationStyle = .formSheet
+                    readerVC.delegate               = self
+                    present(readerVC, animated: true, completion: nil)
+                }
             } else {
                 let alertController = UIAlertController(title: "Locatie", message: "Het lijkt er op dat je niet aanwezig bent bij 't Gildt. Probeer het later nog eens.", preferredStyle: .alert)
                 let defaultAction = UIAlertAction(title: "Annuleer", style: .default, handler: nil)
@@ -107,8 +132,25 @@ class StempelkaartViewController : UIViewController, CLLocationManagerDelegate {
             present(alertController, animated: true, completion: nil)
         }
     }
+    
+    func claimStamp(qrCode: String) {
+        StampAPIService.claimStamp(qrCode: qrCode)
+            .responseData(completionHandler: { [weak self] (response) in
+                DispatchQueue.main.async {
+                    if (response.error == nil) {
+                        self?.getStamps()
+                    } else {
+                        let alertController = UIAlertController(title: "Oeps", message: "Er is iets mis gegaan tijdens het claimen van de stempel...", preferredStyle: .alert)
+                        let defaultAction = UIAlertAction(title: "Annuleer", style: .default, handler: nil)
+                        alertController.addAction(defaultAction)
+                        self!.present(alertController, animated: true, completion: nil)
+                    }
+                }
+            })
+    }
 }
 
+// Card stuff
 extension StempelkaartViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return stamps.count
@@ -120,5 +162,59 @@ extension StempelkaartViewController: UICollectionViewDelegate {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: "StampCollectionViewCell"), for: indexPath) as! StampCollectionViewCell
         cell.item = stamps[indexPath.row]
         return cell
+    }
+}
+
+// QR Stuff
+extension StempelkaartViewController: QRCodeReaderViewControllerDelegate {
+    func reader(_ reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult) {
+        reader.stopScanning()
+        
+        dismiss(animated: true) { [weak self] in
+            self?.claimStamp(qrCode: result.value)
+            let alert = UIAlertController(
+                title: "QRCodeReader",
+                message: String (format:"%@ (of type %@)", result.value, result.metadataType),
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            
+            self?.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func readerDidCancel(_ reader: QRCodeReaderViewController) {
+        reader.stopScanning()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func checkScanPermissions() -> Bool {
+        do {
+            return try QRCodeReader.supportsMetadataObjectTypes()
+        } catch let error as NSError {
+            let alert: UIAlertController
+            
+            switch error.code {
+            case -11852:
+                alert = UIAlertController(title: "Camera", message: "Het lijkt er op dat er geen camera toestemming is gegeven.", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "Instellingen", style: .default, handler: { (_) in
+                    DispatchQueue.main.async {
+                        if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
+                            UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
+                        }
+                    }
+                }))
+                
+                alert.addAction(UIAlertAction(title: "Annuleer", style: .cancel, handler: nil))
+            default:
+                alert = UIAlertController(title: "Camera", message: "QR Reader niet ondersteunt, neem contact op met de ontwikkelaar.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Oke", style: .cancel, handler: nil))
+            }
+            
+            present(alert, animated: true, completion: nil)
+            
+            return false
+        }
     }
 }
