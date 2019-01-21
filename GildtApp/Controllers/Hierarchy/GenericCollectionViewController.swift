b4f99d886f0be5cb8c3b7cde8ef307cc22100243
Jeroen Besse <jeroen.b.k@hotmail.com>
2019-01-21 18:12:54 +0100

    added autorefresh to collectionview

--- GildtApp/Controllers/Hierarchy/GenericCollectionViewController.swift
+++ GildtApp/Controllers/Hierarchy/GenericCollectionViewController.swift
@@ -10,10 +10,17 @@ import Foundation
 import UIKit
 import Alamofire
 
-// TODO: make everything where (https://stackoverflow.com/questions/24089145/multiple-type-constraints-in-swift)
+//basic collectionviewcontroller with functions that all collectionviewcontrollers use
+//if you want to implement/use this:
+//override getCellId & getAPICall in childClass
 class GenericCollectionViewController<T: GenericCollectionViewCell<U>, U>: UICollectionViewController where U: Decodable {
     
     var items = [U]()
+    
+    //timer for autorefresh
+    var autorefreshTimer: Timer?
+    //seconds between each timer tick for autorefresh
+    let autorefreshTimerTickRate = 60.0
     
     func getCellId() -> String {
         print("Error: implement getCellId from GenericCollectionViewController!")
@@ -30,6 +37,26 @@ class GenericCollectionViewController<T: GenericCollectionViewCell<U>, U>: UICol
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        getItems()
+        startAutoRefreshTimer()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        stopAutoRefreshTimer()
+    }
+    
+    func startAutoRefreshTimer() {
+        autorefreshTimer = Timer.scheduledTimer(timeInterval: autorefreshTimerTickRate, target: self, selector: #selector(onTimerTick), userInfo: nil, repeats: true)
+        autorefreshTimer?.tolerance = 0.30
+    }
+    
+    func stopAutoRefreshTimer() {
+        autorefreshTimer?.invalidate()
+    }
+    
+    @objc func onTimerTick(timer: Timer) {
         getItems()
     }
     
