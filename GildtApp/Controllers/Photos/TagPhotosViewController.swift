6b06199f01884e1e6eb00623bd5163a216a1b45e
Jeroen Besse <jeroen.b.k@hotmail.com>
2019-01-25 14:26:36 +0100

    tried reverting everything
    but it still doesn't work??

--- GildtApp/Controllers/Photos/TagPhotosViewController.swift
+++ GildtApp/Controllers/Photos/TagPhotosViewController.swift
@@ -142,30 +142,42 @@ extension TagPhotosViewController {
         
         self.present(alert!, animated: true, completion: nil)
         
-        GildtAPIService.uploadImage(image: image, description: description, tag: tag, callback: photoUploadHandler)
-
+        let baseURL = "https://gildt.inholland-informatica.nl/api/v1"
+        let authToken = LocalStorageService.getAuthToken()
+        let headers: HTTPHeaders = ["Authorization": "Bearer \(authToken)"]
+        let imageData = image.jpegData(compressionQuality: 0.6)
+        
+        Alamofire.upload(multipartFormData: { multipartFormData in
+            multipartFormData.append(imageData!, withName: "image", fileName: "\(Date().timeIntervalSince1970).jpeg", mimeType: "image/jpeg")
+            multipartFormData.append(description.data(using: .utf8)!, withName: "description")
+            multipartFormData.append(String(tag).data(using: .utf8)!, withName: "tags")
+        }, to: URL(string: "\(baseURL)/image")!,
+           method: .post,
+           headers: headers) { encodingResult in
+            switch encodingResult {
+            case .success(let upload, _, _):
+                upload.uploadProgress(closure: { (progress) in
+                    progressView.setProgress(Float(progress.fractionCompleted), animated: true)
+                    self.alert?.message = NSLocalizedString("Photos_Upload_Progress", comment: "") + " \(NSString(format: "%.1f", progress.fractionCompleted * 100))%"
+                    if progress.isFinished {
+                        self.alert?.dismiss(animated: true, completion: {
+                            self.showSuccessMessage()
+                        })
+                    }
+                    if progress.isCancelled {
+                        self.alert?.dismiss(animated: true, completion: {
+                            self.showFailureMessage()
+                        })
+                    }
+                })
+            case .failure(_ ):
+                self.showFailureMessage()
+            }
+        }
     }
     
     func photoUploadHandler(encodingResult: SessionManager.MultipartFormDataEncodingResult) {
-        switch encodingResult {
-        case .success(let upload, _, _):
-            upload.uploadProgress(closure: { (progress) in
-                self.progressView?.setProgress(Float(progress.fractionCompleted), animated: true)
-                self.alert?.message = NSLocalizedString("Photos_Upload_Progress", comment: "") + " \(NSString(format: "%.1f", progress.fractionCompleted * 100))%"
-                if progress.isFinished {
-                    self.alert?.dismiss(animated: true, completion: {
-                        self.showSuccessMessage()
-                    })
-                }
-                if progress.isCancelled {
-                    self.alert?.dismiss(animated: true, completion: {
-                        self.showFailureMessage()
-                    })
-                }
-            })
-        case .failure(_ ):
-            self.showFailureMessage()
-        }
+        
     }
     
     private func showSuccessMessage() {
